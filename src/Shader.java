da0017793463347162896cad5309d8373d87b996
mrwormhole <talhaaltinel@hotmail.com>
2020-03-25 23:56:41 +0000

    transformation for perspective

--- src/Shader.java
+++ src/Shader.java
@@ -1,3 +1,9 @@
+import org.joml.Matrix4f;
+import org.lwjgl.system.MemoryStack;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
@@ -8,14 +14,34 @@ public class Shader {
     private int vertexShaderId;
     private int fragmentShaderId;
 
+    private final Map<String, Integer> uniforms;
+
     public Shader(String filePathVertex, String filePathFragment) throws Exception {
         programId = glCreateProgram();
         if(programId == 0) {
             throw new Exception("Could not create shader!");
         }
+        uniforms = new HashMap<>();
         createVertexShader(Utils.loadResource(filePathVertex));
         createFragmentShader(Utils.loadResource(filePathFragment));
         link();
+        createUniform("projectionMatrix");
+        createUniform("worldMatrix");
+    }
+
+    public void createUniform(String uniformName) throws Exception {
+        int uniformLocation = glGetUniformLocation(programId, uniformName);
+        if (uniformLocation < 0) {
+            throw new Exception("Could not find uniform name called: " + uniformName);
+        }
+        uniforms.put(uniformName, uniformLocation);
+    }
+
+    public void setUniform(String uniformName, Matrix4f value) {
+        // Dump the matrix into a float buffer
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            glUniformMatrix4fv(uniforms.get(uniformName), false, value.get(stack.mallocFloat(16)));
+        }
     }
 
     public void createVertexShader(String shaderCode) throws Exception {
